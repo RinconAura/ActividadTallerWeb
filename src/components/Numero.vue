28e927cc8bd0ca151415ab21329f0cfa08d6a1b6
Sala G9 <CSalaG9@uniboyaca.edu.co>
2023-09-12 20:13:18 -0500

    Commit inicial

---
+++ src/components/Numero.vue
@@ -0,0 +1,13 @@
+<script setup>
+//export default{
+ //   props: {//definir los atributos o propiedades
+//        numero:Object
+//    }
+//}
+const props = defineProps({
+    numero: Object
+});
+</script>
+<template>
+    <p>Numero: {{ numero }}</p>
+</template>
